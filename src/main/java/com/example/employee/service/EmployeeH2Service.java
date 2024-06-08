19e5d5d0528d068dfb65769e58d53acffbd7225d
santhosh <c.santhoshmmc2003@gmail.com>
2024-06-08 05:47:26 +0000

    solution

--- src/main/java/com/example/employee/service/EmployeeH2Service.java
+++ src/main/java/com/example/employee/service/EmployeeH2Service.java
@@ -1,4 +1,5 @@
 package com.example.employee.service;
+
 import com.example.employee.repository.EmployeeRepository;
 import com.example.employee.model.Employee;
 import com.example.employee.model.EmployeeRowMapper;
@@ -20,15 +21,16 @@ public class EmployeeH2Service implements EmployeeRepository {
 
     @Override
     public ArrayList<Employee> getEmployees() {
-        List <Employee> employeeList= db.query("select * from EMPLOYEELIST", new EmployeeRowMapper());
-        ArrayList<Employee> employees=new ArrayList<>(employeeList);
+        List<Employee> employeeList = db.query("select * from EMPLOYEELIST", new EmployeeRowMapper());
+        ArrayList<Employee> employees = new ArrayList<>(employeeList);
         return employees;
     }
 
     @Override
     public Employee getEmployeeById(int employeeId) {
         try {
-            return db.queryForObject("select * from EMPLOYEELIST where employeeId = ?", new EmployeeRowMapper(), employeeId);
+            return db.queryForObject("select * from EMPLOYEELIST where employeeId = ?", new EmployeeRowMapper(),
+                    employeeId);
             return employee;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -37,30 +39,32 @@ public class EmployeeH2Service implements EmployeeRepository {
 
     @Override
     public Employee addEmployee(Employee employee) {
-        db.update("insert into EMPLOYEELIST(employeeName,email,department) values (?,?,?)", employee.getEmployeeName(),
+        db.update("insert into EMPLOYEELIST(employeeName,email,department) values (?,?)", employee.getEmployeeName(),
                 employee.getEmail(), employee.getDepartment());
-        Employee savedEmployee= db.queryForObject("select * from EMPLOYEELIST where employeeName = ? and email = ? and department = ?",
+        Employee savedEmployee = db.queryForObject(
+                "select * from EMPLOYEELIST where employeeName = ? and email = ? and department = ?",
                 new EmployeeRowMapper(), employee.getEmployeeName(), employee.getEmail(), employee.getDepartment());
-                return savedEmployee;
+        return savedEmployee;
     }
+
     @Override
     public Employee updateEmployee(int employeeId, Employee employee) {
         if (employee.getEmployeeName() != null) {
-            db.update("update EMPLOYEELIST set employeeName = ? where employeeId = ?", employee.getEmployeeName(), employeeId);
+            db.update("update EMPLOYEELIST set employeeName = ? where employeeId = ?", employee.getEmployeeName(),
+                    employeeId);
         }
         if (employee.getEmail() != null) {
             db.update("update EMPLOYEELIST set email = ? where employeeId = ?", employee.getEmail(), employeeId);
         }
         if (employee.getDepartment() != null) {
-            db.update("update EMPLOYEELIST set department = ? where employeeId = ?", employee.getDepartment(), employeeId);
+            db.update("update EMPLOYEELIST set department = ? where employeeId = ?", employee.getDepartment(),
+                    employeeId);
         }
         return getEmployeeById(employeeId);
     }
-    
+
     @Override
     public void deleteEmployee(int employeeId) {
         db.update("delete from EMPLOYEELIST where employeeId = ?", employeeId);
     }
 }
-
-
